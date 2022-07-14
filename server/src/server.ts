7ad6b124209f2240aebc1c2b1ff7fd9040712031
csandoval18 <christian_alvites@outlook.com>
2022-07-14 06:42:14 -0500

    set up mongodb connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,11 @@ require('dotenv').config()
 const main = async () => {
 	const app = express()
 	mongoConn()
+
+	const port = process.env.PORT
+	app.listen(port, () => console.log('server started in port:', port))
 }
 
-main()
+main().catch((err) => {
+	console.log(err)
+})
