a2df32128ad1d9238e8e262a3403b8f173291288
Christian AS <alvitesscf18@uww.edu>
2022-08-17 23:18:01 -0500

    implementing movie search

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,18 +1,38 @@
-import React from 'react'
-import {
-	SearchBarContainer,
-	SearchBarInput,
-} from './SearchBar.elements'
+import React, { useEffect, useState } from 'react'
+import { MovieData } from '../../types'
+import { SearchBarContainer, SearchBarInput } from './SearchBar.elements'
 
 const SearchBar = () => {
+	const [movies, setMovies] = useState<MovieData[]>([])
+	const [searchInput, setSearchInput] = useState<String>()
+
+	const searchMovieRequest = async () => {
+		const url = `http://www.omdbapi.com/?s=${searchInput}&apikey=9eaecb1`
+		await fetch(url)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log('data:', data)
+				setMovies(data.Search)
+			})
+	}
+
+	console.log('movies:', movies)
 	return (
 		<SearchBarContainer className='searchbar-container'>
 			<div className='form-control'>
-				<form action=''>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault()
+						searchMovieRequest()
+					}}
+				>
 					<SearchBarInput
 						type='text'
 						placeholder='Search movies'
 						className='input input-bordered max-w-none w-[500px]'
+						onInput={(e) =>
+							setSearchInput((e.target as HTMLInputElement).value)
+						}
 					/>
 				</form>
 			</div>
