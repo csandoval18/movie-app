178aa671d2d61e26ba96962359cbcec1eab30249
csandoval18 <christian_alvites@outlook.com>
2022-08-21 23:53:08 -0500

    added movie-details route and navigation set on movie cards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import HomePage from './components/HomePage/HomePage'
 import Login from './components/Login/Login'
+import MovieDetails from './components/MovieDetails/MovieDetails'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 			<Routes>
 				<Route path='/' element={<HomePage />}></Route>
 				<Route path='/login' element={<Login />} />
+				<Route path='/movie-details' element={<MovieDetails />} />
 			</Routes>
 		</div>
 	)
--- client/src/components/HomePage/MovieList/MovieCard/MovieCard.tsx
+++ client/src/components/HomePage/MovieList/MovieCard/MovieCard.tsx
@@ -2,16 +2,24 @@ import React, { useState } from 'react'
 import { MovieCardContainer } from './MovieCard.elements'
 import { IoMdHeartEmpty, IoMdHeart } from 'react-icons/io'
 import { MoviesSearchData } from '../../../../types'
+import { useNavigate } from 'react-router-dom'
+import { useAppDispatch } from '../../../../app/hooks'
 
 interface MovieCardProps {
 	data: MoviesSearchData
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+	const navigate = useNavigate()
+	const dispatch = useAppDispatch()
 	const [isFavorite, setIsFavorite] = useState(false)
 
 	return (
-		<MovieCardContainer>
+		<MovieCardContainer
+			onClick={() => {
+				navigate('/movie-details')
+			}}
+		>
 			<div className='card card-compact bg-base-100 shadow-xl'>
 				<figure>
 					<img src={data.Poster} alt='poster picture' />
