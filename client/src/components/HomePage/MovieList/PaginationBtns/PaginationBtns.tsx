c305b0af740c64aefa0e77ad802c93a549bb6e59
csandoval18 <christian_alvites@outlook.com>
2022-11-23 18:51:01 -0600

    changing workstation

--- client/src/components/HomePage/MovieList/PaginationBtns/PaginationBtns.tsx
+++ client/src/components/HomePage/MovieList/PaginationBtns/PaginationBtns.tsx
@@ -1,38 +1,25 @@
 import { useEffect, useState } from 'react'
-import { redirect, useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '../../../../app/hooks'
 import {
 	fetchMoviesThunk,
 	selectMovies,
-	selectMovieSearchInput,
 } from '../../../../features/movie/movieSlice'
+import { setPagBtnValues } from '../../../../utils/useSetPagBtnRange'
 import { PaginationBtnsStyle } from '../MoviesList.elements'
-const setPaginationBtnValues = (pageNum: number) => {
-	if (pageNum === 1) {
-		return [1, 2, 3, 4]
-	} else {
-		let page = pageNum
-		let remainder = page % 4
-		let firstPagNum = remainder === 0 ? page - 3 : page - (remainder - 1)
-		console.log('firstpage:', pageNum)
-		return [firstPagNum, firstPagNum + 1, firstPagNum + 2, firstPagNum + 3]
-	}
-}
 
 const PaginationBtns = () => {
 	const navigate = useNavigate()
+	const dispatch = useAppDispatch()
 	const movies = useAppSelector(selectMovies)
 	const { searchInput, pageNum } = useParams()
 	const pageNumInt = parseInt(pageNum as string)
 	const [pageNums, setPageNums] = useState<number[]>(
-		setPaginationBtnValues(pageNumInt),
+		setPagBtnValues(pageNumInt),
 	)
 	const [currPageNum, setCurrPageNum] = useState<number>(
 		pageNum ? parseInt(pageNum) : 1,
 	)
-	const dispatch = useAppDispatch()
-	// Get first number to display for pagination buttons
-
 	const increasePagPages = () => {
 		setPageNums(pageNums.map((curr: number) => curr + 4))
 	}
