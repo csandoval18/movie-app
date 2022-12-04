2e93c8790ccf3c224947907035fbe9741fe3326a
csandoval18 <christian_alvites@outlook.com>
2022-12-03 23:07:08 -0600

    implemented global favorite movies state

--- client/src/features/movie/movieSlice.ts
+++ client/src/features/movie/movieSlice.ts
@@ -4,25 +4,24 @@ import { RootState } from '../../app/store'
 import { MovieDetailsFields, MoviesSearchData } from '../../utils/types'
 import { useSortByYear } from '../../utils/useSortByYear'
 
-export interface SearchMovieState {
+export interface MovieSliceState {
 	searchVal: String
 	movies: MoviesSearchData[]
 	movieDetails: MovieDetailsFields | undefined
+	favorites: []
 	loading: Boolean
 }
-
 interface fetchMoviesArgs {
 	searchVal: String | undefined
 	pageNum?: number
 }
-
-const initialState: SearchMovieState = {
+const initialState: MovieSliceState = {
 	searchVal: '',
 	movies: [],
 	movieDetails: undefined,
+	favorites: [],
 	loading: false,
 }
-
 export const fetchMoviesThunk = createAsyncThunk(
 	'movie/fetchMoviesThunk',
 	async (fetchMoviesArgs: fetchMoviesArgs = { searchVal: '', pageNum: 1 }) => {
@@ -35,33 +34,31 @@ export const fetchMoviesThunk = createAsyncThunk(
 		return movies
 	},
 )
-
 export const fetchMovieDetailsThunk = createAsyncThunk(
 	'movie/fetchMovieDetails',
 	async (fetchMoviesArgs: fetchMoviesArgs) => {
 		const url = `http://www.omdbapi.com/?i=${fetchMoviesArgs.searchVal}&apikey=9eaecb1`
-		let movie = await axios
+		const movie = await axios
 			.get(url)
 			.then((res) => res.data)
 			.catch((err) => console.log(err))
 		return movie
 	},
 )
-
 export const fetchFavoritesThunk = createAsyncThunk(
 	'movie/fetchFavoritesThunk',
 	async () => {
 		const url = 'http://localhost:4000/api/users/favorites'
 		const token = sessionStorage.getItem('token') as string
-		await axios
+		const result = await axios
 			.get(url, {
 				headers: { Authorization: token },
 			})
 			.then((res) => res.data)
 			.catch((err) => console.log(err))
+		return result
 	},
 )
-
 export const movieSlice = createSlice({
 	name: 'MOVIE_SLICE',
 	initialState,
@@ -87,7 +84,6 @@ export const movieSlice = createSlice({
 				state.loading = false
 				state.movies = []
 			})
-
 			// fetchMovieDetailsThunk Cases
 			.addCase(fetchMovieDetailsThunk.pending, (state) => {
 				state.loading = true
@@ -100,12 +96,24 @@ export const movieSlice = createSlice({
 				state.loading = false
 				state.movieDetails = undefined
 			})
+			// fetchFavoritesThunk Cases
+			.addCase(fetchFavoritesThunk.pending, (state) => {
+				state.loading = true
+			})
+			.addCase(fetchFavoritesThunk.fulfilled, (state, action) => {
+				state.loading = false
+				state.movieDetails = action.payload
+			})
+			.addCase(fetchFavoritesThunk.rejected, (state) => {
+				state.loading = false
+				state.movieDetails = undefined
+			})
 	},
 })
-
 export const { setSearchVal, setMovies } = movieSlice.actions
 export const selectMovieSearchInput = (state: RootState) =>
 	state.movie.searchVal
 export const selectMovies = (state: RootState) => state.movie.movies
 export const selectMovieDetails = (state: RootState) => state.movie.movieDetails
+export const selectFavoriteMovies = (state: RootState) => state.movie.favorites
 export default movieSlice.reducer
